d1f06e3096d78c2cb2167a0d1336749b86c55061
adrikaD <adrikad97@gmail.com>
2026-05-25 06:03:17 +0530

    files created

---
+++ common/QuantityCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const QuantityCard = () => {
+  return (
+    <View>
+      <Text>QuantityCard</Text>
+    </View>
+  )
+}
+
+export default QuantityCard
+
+const styles = StyleSheet.create({})
---
+++ components/GoalCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GoalCard = () => {
+  return (
+    <View>
+      <Text>GoalCard</Text>
+    </View>
+  )
+}
+
+export default GoalCard
+
+const styles = StyleSheet.create({})
